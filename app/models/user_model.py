e55fd05ab670f480f4cdec557e92a60b4c58f19f
Ayush Umrao <umraoayush@gmail.com>
2026-08-25 17:49:20 +0530

    feat: add User model using SQLAlchemy ORM

---
+++ app/models/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer,Boolean,String
+from sqlalchemy.orm import Mapped,mapped_column
+from app.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id : Mapped[int] = mapped_column(Integer,primary_key=True,index=True)
+    email:Mapped[str]=mapped_column(String,unique=True,nullable=False,index=True)
+    hashed_password:Mapped[str]=mapped_column(String,nullable=False)
+    is_Active:Mapped[str]=mapped_column(Boolean,default=True)
