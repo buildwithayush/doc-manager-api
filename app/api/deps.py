3b4ff04490c56ab7847c2a3afcabe4e28b848fab
Ayush Umrao <umraoayush@gmail.com>
2026-08-26 16:20:53 +0530

    feat: add get_current_user authentication dependency

---
+++ app/api/deps.py
@@ -0,0 +1,39 @@
+from fastapi import Depends,HTTPException,status
+from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+import jwt
+from jwt.exceptions import InvalidTokenError
+
+from app.database import get_db
+from app.models.user_model import User
+from app.core.security import settings
+
+security_scheme = HTTPBearer()
+
+def get_current_uset(
+        credentials:HTTPAuthorizationCredentials=Depends(security_scheme),
+        db:Session=Depends(get_db)
+) -> User:
+    token = credentials.credentials
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate' :'Bearer'}
+    )
+
+    try:
+      payload = jwt.decode(token,settings.SECRET_KEY,algorithms=[settings.ALGORITHM])
+      user_id: str | None = payload.get("sub")
+      if user_id is None:
+         raise credentials_exception
+      
+    except InvalidTokenError:
+       raise credentials_exception
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None or not user.is_active:
+       raise credentials_exception
+
+    return user
+      
+    
