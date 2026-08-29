02153890322225ab45544943aa0b5072b6e6f0e2
Ayush Umrao <umraoayush@gmail.com>
2026-08-29 16:09:35 +0530

    feat: add Document model and relationship with User

---
+++ app/models/document_model.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
+
+from app.database import Base
+
+if TYPE_CHECKING:
+    from app.models.user_model import User
+
+
+class Document(Base):
+    __tablename__ = 'documents'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    owner: Mapped['User'] = relationship('User', back_populates='documents')
