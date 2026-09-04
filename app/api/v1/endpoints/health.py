17b4fd579488b5fbf985ba5ca220c3c8606f69d6
Ayush Umrao <umraoayush@gmail.com>
2026-09-04 11:47:22 +0530

    feat: add storage health check to health API

--- app/api/v1/endpoints/health.py
+++ app/api/v1/endpoints/health.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from app.services.storage_services import check_storage_health
 
 router = APIRouter()
 
@@ -6,4 +7,12 @@ router = APIRouter()
 def health_check():
     return {
         'status':'ok'
+    }
+
+@router.get('/storgae')
+def storage_health():
+    is_healthy = check_storage_health()
+    return {
+        'storage':'MiniIO S3',
+        'connected':is_healthy
     }
