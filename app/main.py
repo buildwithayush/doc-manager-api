c8ee3359d2ce02e4fb4b6aedbb3e8c0c7b46d951
Ayush Umrao <umraoayush@gmail.com>
2026-09-04 11:47:50 +0530

    feat: implement lifespan context manager for FastAPI with storage initialization

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api.v1.router import router as api_router
 from app.database import Base, engine
+from app.services.storage_services import init_storage,check_storage_health
 
-app =FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    init_storage()
+    yield
+
+
+app =FastAPI(
+    title="Document Management API",
+    description="Enterprise-grade Document & File Management System",
+    lifespan=lifespan
+)
 app.include_router(api_router,prefix='/api/v1')
+
+
