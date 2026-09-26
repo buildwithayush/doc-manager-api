5524a9b98347e30147192a5ff91795e7c5f1d275
Ayush Umrao <umraoayush@gmail.com>
2026-09-27 00:06:32 +0530

    refactor(markdown): remove unnecessary comment in MarkdownChunkService

--- rag/services/markdown_chunk_service.py
+++ rag/services/markdown_chunk_service.py
@@ -1,7 +1,7 @@
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 
 class MarkdownChunkService:
-    # Jin headings par sections break karne hain
+    
     HEADERS_TO_SPLIT_ON = [
         ("#", "Header_1"),
         ("##", "Header_2"),
